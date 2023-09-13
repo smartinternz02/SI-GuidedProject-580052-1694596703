bc78040e21cbbe2fd004e49406b1b2891e8824d1
shivansh0017 <115993253+shivansh0017@users.noreply.github.com>
2023-09-13 19:27:28 +0530

    Add files via upload

---
+++ Assignment_3_ShivanshSrivastava_21BEI0008/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "class5_12092023"
+include(":app")
+ 
